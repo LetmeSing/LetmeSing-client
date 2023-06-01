cc8a05cab1fb5f9d1687fa2e36c930201e7b2a2c
sj-jo <jsj8190@gmail.com>
2023-06-01 22:30:05 +0900

    지도 UI 개선

--- app/src/main/java/com/example/letmesing/MapActivity.java
+++ app/src/main/java/com/example/letmesing/MapActivity.java
@@ -18,6 +18,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -38,7 +39,7 @@ import retrofit2.Response;
 
 import android.Manifest;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, PlaceAdapter.OnItemClickListener {
     private static final int REQUEST_LOCATION_PERMISSION = 1;
     private static final long MIN_TIME_BETWEEN_UPDATES = 1000; // 1초
     private static final float MIN_DISTANCE_CHANGE_FOR_UPDATES = 10.0f; // 10미터
@@ -71,11 +72,26 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         placeAdapter = new PlaceAdapter(placeList); // 객체 생성과 초기화
+        placeAdapter.setOnItemClickListener(this); // 리스너 설정
         recyclerView.setAdapter(placeAdapter);
 
         // API 요청 보내기
         getPlaceListFromServer();
 
+
+    }
+
+    @Override
+    public void onItemClick(int position, TempPlace place) {
+        // 클릭한 위치의 마커를 가져옴
+        Marker marker = getMarkerForPlace(position);
+        if (marker != null) {
+            // 마커의 위치로 카메라 이동
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 15);
+            mMap.animateCamera(cameraUpdate);
+            // 마커 클릭 이벤트 호출하여 InfoWindow 표시
+            marker.showInfoWindow();
+        }
     }
 
     private final Map<String, Marker> markerMap = new HashMap<>();
@@ -98,169 +114,96 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                 if (response.isSuccessful()) {
                     // API 응답이 성공적으로 도착한 경우 장소 목록을 가져와서 처리
                     placeList = response.body();
-                    placeAdapter.setPlaceList(placeList); // 어댑터에 장소 목록 설정
-                    placeAdapter.notifyDataSetChanged(); // RecyclerView 어댑터에 변경 내용을 알려줌
+                    placeAdapter.setPlaceList(placeList); // 어댑터에 목록 설정
+                    placeAdapter.notifyDataSetChanged(); // 어댑터 갱신
 
-                    // 맵이 준비되었을 때 마커 추가
-                    if (mMap != null) {
-                        addMarkersToMap();
+                    // 기존의 마커들을 제거
+                    for (Marker marker : markerMap.values()) {
+                        marker.remove();
+                    }
+                    markerMap.clear();
+
+                    // 새로운 목록의 마커들을 추가
+                    for (int i = 0; i < placeList.size(); i++) {
+                        TempPlace place = placeList.get(i);
+                        LatLng latLng = new LatLng(place.getLatitude(), place.getLongitude());
+                        MarkerOptions markerOptions = new MarkerOptions()
+                                .position(latLng)
+                                .title(place.getName())
+                                .snippet(place.getAddress());
+                        Marker marker = mMap.addMarker(markerOptions);
+                        marker.setTag(place); // 마커의 태그로 TempPlace 객체 설정
+                        markerMap.put(place.getId(), marker);
                     }
                 } else {
-                    // API 요청이 실패한 경우에 대한 처리
-                    Toast.makeText(MapActivity.this, "Failed to fetch place data", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MapActivity.this, "API 요청에 실패했습니다.", Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(@NonNull Call<List<TempPlace>> call, @NonNull Throwable t) {
-                // API 요청 실패에 대한 처리
-                Toast.makeText(MapActivity.this, "Failed to connect to the server", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MapActivity.this, "API 요청에 실패했습니다.", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    // 현재 위치를 가져오는 메서드
-    private LatLng getCurrentLocation() {
-        // 위치 관리자 객체 생성
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        // 위치 권한이 허용되어 있는지 확인
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            // 위치 권한이 허용되어 있다면 현재 위치를 가져옴
-            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if (location != null) {
-                // 현재 위치의 위도와 경도를 LatLng 객체로 변환하여 반환
-                return new LatLng(location.getLatitude(), location.getLongitude());
-            }
-        }
-
-        return null; // 위치를 가져오지 못한 경우 null 반환
-    }
-
+    @SuppressLint("MissingPermission")
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        MapStyleOptions styleOptions = MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style);
-        googleMap.setMapStyle(styleOptions);
 
-        // 기본 마커 비활성화
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        mMap.setMyLocationEnabled(false); // 기본 마커 비활성화
+        // 맵 스타일 설정
+        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style));
 
-        // 현재 위치를 가져옴
-        LatLng currentLocation = getCurrentLocation();
-
-        // 현재 위치가 유효한 경우에만 카메라 이동
-        if (currentLocation != null) {
-            // 카메라를 현재 위치로 이동
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 18));
-            Toast.makeText(MapActivity.this, "카메라를 현재 위치로 이동했습니다.", Toast.LENGTH_SHORT).show();
-        }
-        else {
-            // 지도 초기 위치 설정
-            LatLng karaoke0 = new LatLng(37.507075, 126.958615);
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(karaoke0, 18));
-            Toast.makeText(MapActivity.this, "카메라를 초기 위치로 이동했습니다.", Toast.LENGTH_SHORT).show();
-        }
-        // 서버에서 받은 장소 목록으로 마커 추가
-        addMarkersToMap();
-
-        infoWindowAdapter = new CustomInfoWindowAdapter(this);
-        // 정보창 인터페이스
-        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
-            @Override
-            public View getInfoWindow(@NonNull Marker marker) {
-                return null; // 기본 정보창 사용
+        // 맵을 사용자 현재 위치로 이동
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+        } else {
+            mMap.setMyLocationEnabled(true);
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Criteria criteria = new Criteria();
+            Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+            if (location != null) {
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18));
             }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BETWEEN_UPDATES,
+                    MIN_DISTANCE_CHANGE_FOR_UPDATES, new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+                            // 위치 변경 이벤트 처리
+                        }
 
-            @SuppressLint("SetTextI18n")
-            @Override
-            public View getInfoContents(@NonNull Marker marker) {
-                // 커스텀 정보창을 위한 View 객체 생성
-                @SuppressLint("InflateParams") View infoWindowView = getLayoutInflater().inflate(R.layout.custom_info_window, null);
+                        @Override
+                        public void onStatusChanged(String provider, int status, Bundle extras) {
+                            // 위치 제공자 상태 변경 이벤트 처리
+                        }
 
-                // View 내부의 요소들을 찾아옴
-                TextView titleTextView = infoWindowView.findViewById(R.id.titleTextView);
-                TextView addressTextView = infoWindowView.findViewById(R.id.addressTextView);
-                TextView remainingSeatTextView = infoWindowView.findViewById(R.id.remainingSeatView);
-                TextView totalSeatTextView = infoWindowView.findViewById(R.id.totalSeatView);
-                // 마커에 설정된 태그를 가져옴
-                Object tag = marker.getTag();
+                        @Override
+                        public void onProviderEnabled(String provider) {
+                            // 위치 제공자 활성화 이벤트 처리
+                        }
 
-                if (tag instanceof TempPlace) {
-                    // 마커의 태그로 설정된 TempPlace 객체를 가져옴
-                    TempPlace tempPlace = (TempPlace) tag;
+                        @Override
+                        public void onProviderDisabled(String provider) {
+                            // 위치 제공자 비활성화 이벤트 처리
+                        }
+                    });
+        }
 
-                    // TempPlace 객체에서 이름, 주소, 잔여 좌석 수를 가져와서 View에 표시
-                    titleTextView.setText(tempPlace.getName());
-                    addressTextView.setText(tempPlace.getAddress());
-                    remainingSeatTextView.setText(String.valueOf(tempPlace.getRemainingSeat()));
-                    totalSeatTextView.setText(String.valueOf(tempPlace.getTotalSeat()));
-                }
-
-                return infoWindowView;
-            }
-        });
-
-
-
-        // placeAdapter 객체가 생성된 후에 setOnItemClickListener() 호출
-        placeAdapter.setOnItemClickListener(new PlaceAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(int position, TempPlace place) {
-                // 선택한 장소의 마커를 가져옴
-                Marker marker = getMarkerForPlace(position);
-                if (marker != null) {
-                    // 마커를 클릭한 것처럼 처리하여 InfoWindow를 표시
-                    marker.showInfoWindow();
-
-                    // 마커가 가운데로 오도록 카메라 이동
-                    LatLng latLng = new LatLng(place.getLatitude(), place.getLongitude());
-                    mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
-                }
-            }
-        });
-
-        // 마커 클릭 이벤트 처리
+        // 마커 클릭 리스너 등록
         mMap.setOnMarkerClickListener(this);
+
+        // InfoWindow 어댑터 설정
+        infoWindowAdapter = new CustomInfoWindowAdapter(this);
+        mMap.setInfoWindowAdapter(infoWindowAdapter);
     }
 
-    private void addMarkerToMap(TempPlace place) {
-        double latitude = place.getLatitude();
-        double longitude = place.getLongitude();
-        LatLng latLng = new LatLng(latitude, longitude);
-        MarkerOptions markerOptions = new MarkerOptions()
-                .position(latLng)
-                .title(place.getName())
-                .snippet(place.getAddress());
-        Marker marker = mMap.addMarker(markerOptions);
-        assert marker != null;
-        marker.setTag(place);
-        markerMap.put(place.getId(), marker);
-    }
-
-    private void addMarkersToMap() {
-        mMap.clear();
-        for (TempPlace place : placeList) {
-            addMarkerToMap(place);
-        }
-    }
     @Override
-    public boolean onMarkerClick(final Marker marker) {
-        // 마커를 클릭했을 때 팝업 정보 띄우기
+    public boolean onMarkerClick(Marker marker) {
+        // 마커 클릭 시 InfoWindow 표시
         marker.showInfoWindow();
         return true;
     }
-
-
 }
