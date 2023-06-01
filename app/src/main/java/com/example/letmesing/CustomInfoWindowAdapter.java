4e6ea37f70039f5ce8a31040af4b7db8ecd0ba91
sj-jo <jsj8190@gmail.com>
2023-06-01 22:30:05 +0900

    Fix: 지도 UI 개선

--- app/src/main/java/com/example/letmesing/CustomInfoWindowAdapter.java
+++ app/src/main/java/com/example/letmesing/CustomInfoWindowAdapter.java
@@ -1,23 +1,17 @@
 package com.example.letmesing;
 
 import android.content.Context;
-import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.core.content.ContextCompat;
-
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
-
-import org.w3c.dom.Text;
 
 public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
     private final Context mContext;
     private final LayoutInflater mInflater;
-
 
     public CustomInfoWindowAdapter(Context context) {
         mContext = context;
@@ -26,6 +20,7 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
     @Override
     public View getInfoWindow(Marker marker) {
+        // 기본 InfoWindow를 사용하도록 null 반환
         return null;
     }
 
@@ -45,8 +40,19 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
         // 마커 정보 설정
         String title = marker.getTitle();
         String address = marker.getSnippet();
-        String remainingSeat = marker.getTag().toString();
-        String totalSeat = marker.getTag().toString();
+        String remainingSeat = "";
+        String totalSeat = "";
+
+        Object tag = marker.getTag();
+        if (tag != null) {
+            if (tag instanceof TempPlace) {
+                TempPlace place = (TempPlace) tag;
+                remainingSeat = String.valueOf(place.getRemainingSeat());
+                totalSeat = String.valueOf(place.getTotalSeat());
+            } else {
+                // 태그가 다른 형식일 경우에 대한 처리
+            }
+        }
 
         titleTextView.setText(title);
         addressTextView.setText(address);
--- app/src/main/java/com/example/letmesing/PlaceAdapter.java
+++ app/src/main/java/com/example/letmesing/PlaceAdapter.java
@@ -1,6 +1,13 @@
 package com.example.letmesing;
 
+
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,13 +15,20 @@ import android.widget.AdapterView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
+import android.Manifest;
+
 public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.PlaceViewHolder> {
 
     private List<TempPlace> placeList;
+    private OnItemClickListener itemClickListener;
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private static final int ACCESS_FINE_LOCATION = 2;
+    private static final int ACCESS_COARSE_LOCATION = 3;
 
     public PlaceAdapter(List<TempPlace> placeList) {
         this.placeList = placeList;
@@ -23,14 +37,15 @@ public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.PlaceViewHol
     public void setPlaceList(List<TempPlace> placeList) {
         this.placeList = placeList;
     }
-    private OnItemClickListener itemClickListener;
 
     public void setOnItemClickListener(OnItemClickListener listener) {
         this.itemClickListener = listener;
     }
+
     public interface OnItemClickListener {
         void onItemClick(int position, TempPlace place);
     }
+
     @NonNull
     @Override
     public PlaceViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -43,16 +58,38 @@ public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.PlaceViewHol
         TempPlace tempPlace = placeList.get(position);
         holder.titleTextView.setText(tempPlace.getName());
         holder.addressTextView.setText(tempPlace.getAddress());
-        //holder.totalSeatView.setText(String.valueOf(tempPlace.getTotalSeat()) + "자리");
         holder.remainingSeatView.setText("남은 자리 : " + String.valueOf(tempPlace.getRemainingSeat()));
 
-        //holder.placeImageView.setImageResource(place.getPhotoResId());
-        // 아이템 클릭 이벤트 처리
+        // 현재 위치 가져오기
+        LocationManager locationManager = (LocationManager) holder.itemView.getContext().getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        if (ActivityCompat.checkSelfPermission(holder.itemView.getContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(holder.itemView.getContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions((Activity) holder.itemView.getContext(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+        }
+
+        Location location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria, false));
+
+        if (location != null) {
+            // 마커와 현재 위치 간의 거리 계산
+            float[] distance = new float[1];
+            Location.distanceBetween(location.getLatitude(), location.getLongitude(), tempPlace.getLatitude(), tempPlace.getLongitude(), distance);
+            float distanceInMeters = distance[0] / 1000;
+            double distanceInKMeters = (Math.round(distanceInMeters * 100) /100.0);
+            // 거리 표시
+            holder.distanceTextView.setText(distanceInKMeters + "km");
+        }
+
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (itemClickListener != null) {
                     itemClickListener.onItemClick(position, tempPlace);
+
+                    // Request location permission
+                    if (ActivityCompat.checkSelfPermission(holder.itemView.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                            && ActivityCompat.checkSelfPermission(holder.itemView.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions((Activity) holder.itemView.getContext(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+                    }
                 }
             }
         });
@@ -67,17 +104,24 @@ public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.PlaceViewHol
         private TextView titleTextView;
         private TextView addressTextView;
         private TextView remainingSeatView;
-        //private TextView totalSeatView;
+        private TextView distanceTextView;
 
         public PlaceViewHolder(@NonNull View itemView) {
             super(itemView);
             titleTextView = itemView.findViewById(R.id.titleTextView);
             addressTextView = itemView.findViewById(R.id.snippetTextView);
-            //totalSeatView = itemView.findViewById(R.id.totalSeatView);
             remainingSeatView = itemView.findViewById(R.id.remainingSeatView);
+            distanceTextView = itemView.findViewById(R.id.distanceTextView);
 
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    if (position != RecyclerView.NO_POSITION && itemClickListener != null) {
+                        itemClickListener.onItemClick(position, placeList.get(position));
+                    }
+                }
+            });
         }
     }
-
-
 }
